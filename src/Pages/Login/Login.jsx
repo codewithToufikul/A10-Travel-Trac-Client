ec1d550cb8fb32631ab776ad721e2a9d4e594ad3
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-04-26 17:38:59 +0600

    login page added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,10 @@
+import { FaGithub } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+
 const Login = () => {
   return (
     <div className="max-w-[1440px] flex justify-center mx-auto">
-      <div className=" mt-8 w-[800px] p-5 bg-gray-100">
+      <div className=" mt-8 w-[800px] p-10 bg-gray-100">
         <h1 className=" text-4xl text-center font-semibold ">
           Login Your <span className=" text-blue-400">Account</span>
         </h1>
@@ -29,12 +32,13 @@ const Login = () => {
           />
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn bg-blue-400 text-lg text-white">Login</button>
         </div>
         <div className="divider">OR</div>
         <div>
-            <div>
-                <button className="btn text-lg">Google</button>
+            <div className=" flex justify-center gap-6">
+                <button className="btn hover:shadow-2xl hover:text-black bg-transparent hover:bg-transparent btn-outline btn-wide text-4xl px-5"><FcGoogle /> <span className=" text-xl">Google</span></button>
+                <button className="btn hover:shadow-2xl bg-transparent hover:bg-transparent hover:text-black btn-outline btn-wide text-4xl px-5"><FaGithub /> <span className=" text-xl">GitHub</span></button>
             </div>
         </div>
       </form>
