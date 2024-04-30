f44aa048501505424967e5f72c8e0506cfe3b990
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-04-30 10:37:32 +0600

    navbar sticky on top

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -97,7 +97,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar justify-between bg-base-100 max-w-[1440px] mx-auto py-4">
+    <div className="navbar sticky top-0 z-50 justify-between bg-base-100 max-w-[1440px] mx-auto py-4">
       <div className="navbar-start md:w-[20%]">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
